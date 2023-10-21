d2165f845e1c4bb035001cbf26e5c91f76b9a55a
Jonathan <56273460+Jonathan-Jay@users.noreply.github.com>
2023-10-21 18:15:40 -0400

    bike has smonk

--- ShrimpUnityProject/Assets/Scripts/PizzaManager.cs
+++ ShrimpUnityProject/Assets/Scripts/PizzaManager.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 
 public class PizzaManager : MonoBehaviour
 {
-	
-
+	public ParticleSystem particles;
+	public Material sharedSmoke;
+	public Material sharedFire;
 	void OnParticleCollision(GameObject other) {
-		if (other.transform.parent != transform) {
+		if (other != particles.gameObject) {
 			
+		}
+	}
+
+	public void SetJalepenio() {
+		particles.GetComponent<ParticleSystemRenderer>().sharedMaterial = sharedFire;
+		ParticleSystem.MainModule main = particles.main;
+		main.startLifetime = 2f;
+	}
+
+	public void RemoveJalepenio() {
+		particles.GetComponent<ParticleSystemRenderer>().sharedMaterial = sharedSmoke;
+		ParticleSystem.MainModule main = particles.main;
+		main.startLifetime = 1f;
+	}
+
+	private void Start() {
+		if (Random.Range(0, 2) > 0) {
+			SetJalepenio();
 		}
 	}
 }
