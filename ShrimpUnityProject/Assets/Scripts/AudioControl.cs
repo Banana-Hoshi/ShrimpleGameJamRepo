6cf3375c739a6efcc6e3fb4a3dc9af8714940884
Duccks330 <139393379+Duccks330@users.noreply.github.com>
2023-10-22 11:32:53 -0400

    audio controls added

---
+++ ShrimpUnityProject/Assets/Scripts/AudioControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    public void SetMusicVolume(float val) {
+        
+    }
+    public void SetSFXVolume(float val) {
+        
+    }
+    public void SetMasterVolume(float val) {
+        
+    }
+}
